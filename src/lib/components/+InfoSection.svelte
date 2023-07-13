a630cb3c0fa2d8c6e8d47a45548026e530d25573
neuralworm <nerualworm@protonmail.com>
2023-07-13 12:40:15 -0700

    styling

--- src/lib/components/+InfoSection.svelte
+++ src/lib/components/+InfoSection.svelte
@@ -1,9 +1,9 @@
 <section class="w-64 mt-8">
-    <div id="about-section">
-        <p>
+    <div id="about-section" class="leading-6">
+        <p class="leading-">
             Use your <strong>keyboard arrows</strong> (for Desktop/Laptop) or <strong>swipe</strong> (for mobile) on the game board to move the blocks in the corresponding direction.  If tiles of identical values touch, they <strong>combine</strong> and grow larger.  Try to make as high a number as possible. 
         </p>
-        <p class="mt-4">
+        <p class="mt-4 leading-">
             I am not the original creator of the game <strong>2048</strong>, and credit in that regard goes to <em>Gabriele Cirulli.</em>
         </p>
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
     <meta name="description" content="Play 2048 in your broowser for free.  Check leaderboards and previous high scores.">
 </svelte:head>
 <section id="title-view" class="">
-    <h4>IT'S</h4>
-    <h1 class="text-5xl font-bold">2048!</h1>
+    <h4 class="font-bold text-neutral-100 tracking-tight bg-blue-500 w-10 h-10 flex items-center justify-center rounded-md">IT'S</h4>
+    <h1 class="text-5xl font-bold tracking-tight font-extrabold text-transparent  bg-clip-text bg-gradient-to-r from-white to-neutral-600">2048!</h1>
 </section>
 <section id="game-view-container" class="mt-12">
     <GameView />
