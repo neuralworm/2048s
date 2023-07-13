9c7f99b1dca15f48a0eb8e09738660c1e685028a
neuralworm <nerualworm@protonmail.com>
2023-07-13 11:14:31 -0700

    Changed name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,12 @@
 
 </script>
 <svelte:head>
-    <title>Play 2048</title>
+    <title>It's 2048!</title>
     <meta name="description" content="Play 2048 in your broowser for free.  Check leaderboards and previous high scores.">
 </svelte:head>
 <section id="title-view" class="">
-    <h4>PLAY</h4>
-    <h1 class="text-5xl font-bold">2048</h1>
+    <h4>IT'S</h4>
+    <h1 class="text-5xl font-bold">2048!</h1>
 </section>
 <section id="game-view-container" class="mt-12">
     <GameView />
