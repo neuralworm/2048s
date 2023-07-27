49c66b84ca08e9f35ef0f06fd7976bd0162f9a8b
neuralworm <nerualworm@protonmail.com>
2023-07-27 10:31:57 -0700

    desc and favicon.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,8 @@
 </script>
 <svelte:head>
     <title>It's 2048!</title>
-    <meta name="description" content="Play 2048 in your broowser for free.  Check leaderboards and previous high scores.">
+    <meta name="description" content="Play 2048 in your browser for free.  Check leaderboards and previous high scores.">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📱</text></svg>">
 </svelte:head>
 <section id="title-view" class="">
     <h4 class="font-bold text-neutral-100 tracking-tight bg-blue-500 w-10 h-10 flex items-center justify-center rounded-md">IT'S</h4>
