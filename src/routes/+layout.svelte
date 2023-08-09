24434ef787e8a6ab11a491dee9300769c503e8dc
neuralworm <jakemillerdev@gmail.com>
2023-08-09 15:26:07 -0700

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
+    import { dev } from "$app/environment";
     import "../app.css";
+    import { inject } from '@vercel/analytics';
+  inject({
+    mode: dev ? 'development' : 'production'
+  })
   </script>
   <div class="mx-auto max-w-xl py-8 w-72">
 
